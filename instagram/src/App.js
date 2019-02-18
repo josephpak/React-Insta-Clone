7feb5928cdc6d4959dd39314a2fb476780aff91e
joepak <josephpak2014@gmail.com>
2019-02-18 16:25:38 -0500

    Basic outline

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import dummyData from './dummy-data';
+import './reset.css';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      postdata: {dummyData}
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <div>Image Logo</div>
+          <div>Text Logo</div>
+          <div>Search Bar</div>
+          <div>Image</div>
+          <div>Image</div>
+          <div>Image</div>
         </header>
+        <div className="posts-container">
+          <div className="post-container">
+            <div className="user-content-container">User Post</div>
+            <div className="comment-section-container">Comment Section</div>
+          </div>      
+        </div>
       </div>
     );
   }
