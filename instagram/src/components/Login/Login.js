e7f6fdff3dda4a3c2d1ba42a6f3f3b28a900ba38
joepak <josephpak2014@gmail.com>
2019-02-20 15:44:22 -0500

    Login Page buildout

---
+++ instagram/src/components/Login/Login.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class Login extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleInput = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    login = () => {
+        e.preventDefault();
+        localStorage.setItem("username", this.state.username);
+        window.location.reload();
+    }
+
+    render() {
+        return (
+            <>
+                <form onSubmit={this.login}>
+                    <input 
+                    type="text"
+                    name="username"
+                    onChange={this.handleInput}
+                    />
+                    <input 
+                    type="text"
+                    name="password"
+                    onChange={this.handleInput}
+                    />
+                    <button>Login</button>
+                </form>  
+            </>
+        )
+    }
+}
